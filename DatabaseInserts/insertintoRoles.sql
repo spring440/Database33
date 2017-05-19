f1ddce8ab4e392f33e03a86ee8821fcb30375f68
Bryant <BryantBarron@users.noreply.github.com>
2017-05-18 18:13:35 -0700

    Add files via upload

---
+++ DatabaseInserts/insertintoRoles.sql
@@ -0,0 +1,7 @@
+insert into role(role_description) values ('presenter');
+insert into role(role_description) values ('attendee');
+insert into role(role_description) values ('organizer');
+insert into role(role_description) values ('volunteer');
+insert into role(role_description) values ('sponsor');
+
+
